bb11b6ae7d71583cbdf298bec717c3758ae9f14f
huangzewei@wiz.top <huangzewei@wiz.top>
2020-02-01 02:10:10 +0800

    修改域登录的逻辑

--- src/MaterialCodeSelectionPlatform/MaterialCodeSelectionPlatform.Web/Controllers/LoginController.cs
+++ src/MaterialCodeSelectionPlatform/MaterialCodeSelectionPlatform.Web/Controllers/LoginController.cs
@@ -39,16 +39,16 @@ namespace Top.WizEasc.ManagerWeb.Controllers
         public async Task<IActionResult> LoginUserName(string userName, string password)
         {
 
-            var user = await userService.GetByUserNamePwd(userName, CommonHelper.ToMD5(password));
-            if (user == null)
+            ///域登录一般的用户名形式为
+            ///  domain\userName  或者 userName@domain
+            if (userName.Contains("@") || userName.Contains("\\"))
             {
-                //判断是否为域账户登录
                 if (LDAPUtil.Validate(userName, password))
                 {
                     var list = await userService.GetByParentId("DomainUserName", userName);
                     if (list.Count > 0)
                     {
-                        user = list.FirstOrDefault();
+                        var user = list.FirstOrDefault();
                         var claimsIdentity = new ClaimsIdentity(new[] {new Claim(ClaimTypes.Name, user.Id),
                             new Claim("LoginName", user.LoginName),
                             new Claim("UserName", user.Name),
@@ -67,19 +67,71 @@ namespace Top.WizEasc.ManagerWeb.Controllers
             }
             else
             {
-                var claimsIdentity = new ClaimsIdentity(new[] {new Claim(ClaimTypes.Name, user.Id),
+
+                var user = await userService.GetByUserNamePwd(userName, CommonHelper.ToMD5(password));
+                if (user == null)
+                {
+                    return Content("用户名或密码错误");
+                }
+                else
+                {
+                    var claimsIdentity = new ClaimsIdentity(new[] {new Claim(ClaimTypes.Name, user.Id),
                     new Claim("LoginName", user.LoginName),
                     new Claim("UserName", user.Name),
                 }, "Basic");
 
-                var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
-                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
+                    var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
 
-                HttpContext.Session.SetString("LoginName", user.LoginName ?? Guid.Empty.ToString());
-                HttpContext.Session.SetString("UserName", user.Name ?? Guid.Empty.ToString());
-                HttpContext.Session.SetString("UserId", user.Id ?? Guid.Empty.ToString());
-                return Content("success");
+                    HttpContext.Session.SetString("LoginName", user.LoginName ?? Guid.Empty.ToString());
+                    HttpContext.Session.SetString("UserName", user.Name ?? Guid.Empty.ToString());
+                    HttpContext.Session.SetString("UserId", user.Id ?? Guid.Empty.ToString());
+                    return Content("success");
+                }
             }
+
+
+            //var user = await userService.GetByUserNamePwd(userName, CommonHelper.ToMD5(password));
+            //if (user == null)
+            //{
+            //    //判断是否为域账户登录
+            //    if (LDAPUtil.Validate(userName, password))
+            //    {
+            //        var list = await userService.GetByParentId("DomainUserName", userName);
+            //        if (list.Count > 0)
+            //        {
+            //            user = list.FirstOrDefault();
+            //            var claimsIdentity = new ClaimsIdentity(new[] {new Claim(ClaimTypes.Name, user.Id),
+            //                new Claim("LoginName", user.LoginName),
+            //                new Claim("UserName", user.Name),
+            //            }, "Basic");
+
+            //            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+            //            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
+
+            //            HttpContext.Session.SetString("LoginName", user.LoginName ?? Guid.Empty.ToString());
+            //            HttpContext.Session.SetString("UserName", user.Name ?? Guid.Empty.ToString());
+            //            HttpContext.Session.SetString("UserId", user.Id ?? Guid.Empty.ToString());
+            //            return Content("success");
+            //        }
+            //    }
+            //    return Content("用户名或密码错误");
+            //}
+            //else
+            //{
+            //    var claimsIdentity = new ClaimsIdentity(new[] {new Claim(ClaimTypes.Name, user.Id),
+            //        new Claim("LoginName", user.LoginName),
+            //        new Claim("UserName", user.Name),
+            //    }, "Basic");
+
+            //    var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+            //    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
+
+            //    HttpContext.Session.SetString("LoginName", user.LoginName ?? Guid.Empty.ToString());
+            //    HttpContext.Session.SetString("UserName", user.Name ?? Guid.Empty.ToString());
+            //    HttpContext.Session.SetString("UserId", user.Id ?? Guid.Empty.ToString());
+            //    return Content("success");
+            //}
         }
 
 
