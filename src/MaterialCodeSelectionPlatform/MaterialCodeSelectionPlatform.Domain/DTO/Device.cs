255f0df80d0d1d5e4e8bc1d110b282c4afb563da
hanshuai <hanshuai@wiz.top>
2020-02-19 00:17:04 +0800

    修改详情页面样式，修改选码搜索样式

--- src/MaterialCodeSelectionPlatform/MaterialCodeSelectionPlatform.Domain/DTO/Device.cs
+++ src/MaterialCodeSelectionPlatform/MaterialCodeSelectionPlatform.Domain/DTO/Device.cs
@@ -9,5 +9,11 @@ namespace MaterialCodeSelectionPlatform.Domain.Entities
         /// </summary>
         [SugarColumn(IsIgnore = true)]
         public string ProjectName { get; set; }
+
+        /// <summary>
+        /// 项目名称
+        /// </summary>
+        [SugarColumn(IsIgnore = true)]
+        public string ProjectCode { get; set; }
     }
 }
